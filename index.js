80f668f36453cb9245921924e5ff46df9a0eef32
Muyivu Shafiq <cybersharph@gmail.com>
2022-09-15 04:54:18 +0400

    Added Save & Clear History Features

--- index.js
+++ index.js
@@ -2,24 +2,45 @@
 
 let number = 0;
 
-
 function Increment() {
     document.getElementById("number").innerText = number + 1;
     number++;
 }
 
 function Decrement() {
-    // if helps to not decrement zeros towards negative
+    // it helps to not decrement zeros towards negative
     if (number > 0) {
         document.getElementById("number").innerText = number - 1;
         number--;
     }
 }
 
-function Hidden() {
+function Reset() {
     number = 0; // reset number variable to 0;
     document.getElementById("number").innerText = number;
 }
 
 
+// Tracking saves
+function Save(){
+    if(number > 0){
+        recentEl = document.getElementById("recent");
+    // Check if the html element is empty
+        if (recentEl.childNodes.length === 0) {
+            recentEl.innerText = number;
+        } 
+        else {
+            recentEl.innerText += " - " + number;
+        }
+    } else {
+        alert("You can't save a ZERO");
+        // A better way will go here
+    }
+}
 
+// Clear saves 
+
+function Clear() {
+    Reset();
+    recentEl.innerText = "";
+}
